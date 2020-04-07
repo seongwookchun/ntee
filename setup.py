e16652ec5a2c7915c21949384fa655bb42b2ae73
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2020-04-07 22:12:26 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='ntee',
-    version='0.0.1.7',
+    version='0.0.1.8',
     author='Studio Ousia',
     author_email='ikuya@ousia.jp',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'ntee=ntee.cli:cli'
-        ]
+    #entry_points={
+    #    'console_scripts': [
+    #        'ntee=ntee.cli:cli'
+    #    ]
     }
 )
